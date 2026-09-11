b09ddd9ddbb910486d6613c7153fc90f66906a2e
Gottipilli Soma Sekhar <mylabs_somasekhar1.m1.com.sg>
2026-09-11 16:02:50 +0800

    draft version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,5 @@
-import Hero from "@/components/home/Hero";
-import TrustStrip from "@/components/home/TrustStrip";
-import PropertyFinder from "@/components/home/PropertyFinder";
-import FeaturedProperties from "@/components/home/FeaturedProperties";
-import WhyVizag from "@/components/home/WhyVizag";
-import ServicesPreview from "@/components/home/ServicesPreview";
-import NRISection from "@/components/home/NRISection";
-import InvestmentBrief from "@/components/home/InvestmentBrief";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main>
-      <Hero />
-      <TrustStrip />
-      <PropertyFinder />
-      <FeaturedProperties />
-      <WhyVizag />
-      <ServicesPreview />
-      <NRISection />
-      <InvestmentBrief />
-    </main>
-  );
+  redirect("/welcome");
 }
